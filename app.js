7a91382b8b036517ed6bfaadb822b6e798f4c7f5
imtiaz ali <adv.imtiazpanhwar@gmail.com>
2024-03-11 17:44:50 +0500

    assignment 05

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+//Qoute from famous person
+let famous_Person = "Albert Einstine";
+let message = "A person who never made a mistake never tried any thing new";
+console.log(`${famous_Person} once said, ${message}`);
---
+++ app.ts
@@ -0,0 +1,7 @@
+//Qoute from famous person
+
+let famous_Person:string = "Albert Einstine";
+
+let message:string = "A person who never made a mistake never tried any thing new";
+
+console.log(`${famous_Person} once said, ${message}`);
